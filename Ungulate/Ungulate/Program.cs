0821dfe80227cdc698ccd4d788cf27ec15182e08
Matt Wissman <mwissman@rewardsnetwork.com>
2014-11-15 15:54:25 -0600

    owin setup

--- Ungulate/Ungulate/Program.cs
+++ Ungulate/Ungulate/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using Owin;
 
 namespace Ungulate
 {
@@ -10,6 +14,48 @@ namespace Ungulate
     {
         static void Main(string[] args)
         {
+            string baseAddress = "http://localhost:9080/";
+
+            
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                Console.WriteLine("Ungulate is running");
+                Console.WriteLine("Press any key to close...");
+                Console.ReadLine();
+            }
+
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<TopLevelModule>();
+            var container = builder.Build();
+            
+
+            // Register the Autofac middleware FIRST.
+            app.UseAutofacMiddleware(container);
+
+            app.Use<FooMiddleWare>();
+        }
+    }
+
+    public class FooMiddleWare: OwinMiddleware
+    {
+        public FooMiddleWare(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public async override Task Invoke(IOwinContext context)
+        {
+            var path = context.Request.Path;
+
+            context.Response.Write("path:"+path);
+
+            await Next.Invoke(context);
         }
     }
 }
---
+++ Ungulate/Ungulate/TopLevelModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Ungulate
+{
+    public class TopLevelModule :Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}

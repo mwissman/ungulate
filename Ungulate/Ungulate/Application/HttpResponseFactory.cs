9fec61d60bf32d788375dc4c6fd1c6ff83031e4e
Matt Wissman <mwissman@rewardsnetwork.com>
2014-11-25 09:40:04 -0600

    spiking Http Response Appliers

--- Ungulate/Ungulate/Application/HttpResponseFactory.cs
+++ Ungulate/Ungulate/Application/HttpResponseFactory.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Owin;
 using Ungulate.Domain;
 
 namespace Ungulate.Application
@@ -6,7 +10,84 @@ namespace Ungulate.Application
     {
         public IHttpResponse Create(Mapping mapping)
         {
-            throw new System.NotImplementedException();
+            var status = new StatusHttpResponse(mapping);
+            var headers = new HeadersHttpResponse(mapping);
+            var body = new BodyHttpResponse(mapping);
+
+            IList<IHttpResponse> responseAppliers = new List<IHttpResponse>()
+            {
+                status,
+                headers,
+                body,
+            };
+
+            return new AllHttpResponse(responseAppliers);
+        }
+    }
+
+    public class AllHttpResponse : IHttpResponse
+    {
+        private readonly IList<IHttpResponse> _responseAppliers;
+
+        public AllHttpResponse(IList<IHttpResponse> responseAppliers)
+        {
+            _responseAppliers = responseAppliers;
+        }
+
+        public void ApplyTo(IOwinResponse owinResponse)
+        {
+            foreach (var responseApplier in _responseAppliers)
+            {
+                responseApplier.ApplyTo(owinResponse);
+            }
+        }
+    }
+
+    public class StatusHttpResponse : IHttpResponse
+    {
+        private readonly Mapping _mapping;
+
+        public StatusHttpResponse(Mapping mapping)
+        {
+            _mapping = mapping;
+        }
+
+        public void ApplyTo(IOwinResponse owinResponse)
+        {
+            owinResponse.StatusCode = _mapping.Response.Status;
+        }
+    }
+    
+    public class HeadersHttpResponse : IHttpResponse
+    {
+        private readonly Mapping _mapping;
+
+        public HeadersHttpResponse(Mapping mapping)
+        {
+            _mapping = mapping;
+        }
+
+        public void ApplyTo(IOwinResponse owinResponse)
+        {
+            foreach (var header in _mapping.Response.Headers)
+            {
+                owinResponse.Headers.Add(header.Key,new []{header.Value});
+            }
+        }
+    }
+
+    public class BodyHttpResponse : IHttpResponse
+    {
+        private readonly Mapping _mapping;
+
+        public BodyHttpResponse(Mapping mapping)
+        {
+            _mapping = mapping;
+        }
+
+        public void ApplyTo(IOwinResponse owinResponse)
+        {
+           throw new NotImplementedException();
         }
     }
 }
